e9ed84c2aecfee0ad4ede8e68e2a21ae9ff70457
Serg Malinovsky <sMalinovsky32768@gmail.com>
2021-06-25 16:40:18 +0300

    9

---
+++ Ninth/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace Ninth
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            WriteLine("Hello World!");
+            Method();
+            WriteLine("Goodbuy World!");
+        }
+
+        public static void Method()
+        {
+            WriteLine(RandomNumberGenerator.GetInt32(int.MaxValue));
+            new Thread(new ThreadStart(Method)).Start();
+        }
+    }
+}
